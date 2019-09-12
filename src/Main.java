3e522864a3158fd735ba037fb4eb46528a561657
Arattan <amanrattan21@gmail.com>
2019-09-12 14:09:45 +0200

    Add FileNotFile Exception to Main Class

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
+import java.io.FileNotFoundException;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
 
         //TODO Implement an Input Scanner which reads a player's guess(es)
         //TODO Implement the while loop measuring
         //TODO Need to create a game object on start up which randomly chooses a movie from the list
         //TODO Where will the movie list be kept? The Game should have a File Scanner to find the names from the list.
         System.out.println("Welcome to Guess The Movie!");
-        Game game = new Game();
+
+        try{
+            Game game = new Game();
+        } catch (FileNotFoundException exception){
+            throw exception;
+        }
     }
 }
