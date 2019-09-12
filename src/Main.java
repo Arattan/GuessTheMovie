b6fae90cbea62718ab294fd50ea10df482929e63
Arattan <amanrattan21@gmail.com>
2019-09-12 13:45:15 +0200

    Add Main and Game Classes

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args){
 
+        //TODO Implement an Input Scanner which reads a player's guess(es)
+        //TODO Implement the while loop measuring
+        //TODO Need to create a game object on start up which randomly chooses a movie from the list
+        //TODO Where will the movie list be kept? The Game should have a File Scanner to find the names from the list.
+        System.out.println("Welcome to Guess The Movie!");
     }
 }
