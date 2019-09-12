555f5a2add5ae34a47bae4b91fc108798a670ca9
Arattan <amanrattan21@gmail.com>
2019-09-12 14:11:34 +0200

    Implement hasNextLine loop to check output of all movies in the list

--- src/Game.java
+++ src/Game.java
@@ -6,7 +6,12 @@ public class Game{
     String name;
 
     public Game() throws FileNotFoundException {
+
         File movies = new File("movies.txt");
         Scanner fileScanner = new Scanner(movies);
+
+        while(fileScanner.hasNextLine()){
+            System.out.println(fileScanner.nextLine());
+        }
     }
 }
