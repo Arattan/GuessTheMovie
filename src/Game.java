8f7d8d653f9daefa0f2cb3c892851993669b4cb7
Arattan <amanrattan21@gmail.com>
2019-09-12 16:02:46 +0200

    Add random number variable to pick a listed movie

--- src/Game.java
+++ src/Game.java
@@ -9,6 +9,7 @@ public class Game{
 
         File movies = new File("movies.txt");
         Scanner fileScanner = new Scanner(movies);
+        int movie = (int) (Math.random() * 24) + 1;
 
         while(fileScanner.hasNextLine()){
             System.out.println(fileScanner.nextLine());
