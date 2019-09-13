67f52fd3de521dc0eecc423696b56601f1b59849
Arattan <amanrattan21@gmail.com>
2019-09-13 19:53:31 +0200

    Add masking code to replace movie title chars with _

--- src/Game.java
+++ src/Game.java
@@ -10,15 +10,21 @@ public class Game{
                             //File movies = new File("movies.txt");
                             //Scanner fileScanner = new Scanner(movies);
                                 // String [] args = {fileScanner.nextLine()};
-
         int randNum = (int) (Math.random() * 4);
         System.out.println(randNum);
         String movie = this.movies[randNum];
         System.out.println(movie);
         int titleLength = movie.length();
         System.out.println(titleLength);
+        String maskedString = movie.replaceAll("[a-zA-Z0-9]","_");
+        System.out.println(maskedString);
                         // System.out.println(fileScanner.nextLine());
     }
+
+
+
+
+
 
 
         //int movieName = movie.split(" ").length;
